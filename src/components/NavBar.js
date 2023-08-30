2c451fff47bf68332855769966be734fbd0b66bb
Harshada Chaudhari <charsh18@gmail.com>
2023-08-30 12:18:32 +0200

    removed comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,23 +10,15 @@ const Navbar = () => {
 
         <ul className='navbar-list'>
           <li>
-            {/* <NavLink exact to="/" activeClassName="active">About</NavLink> */}
             <a href="#about">About</a>
           </li>
           <li>
-            {/* <NavLink to="/skills" activeClassName="active">Skills</NavLink> */}
             <a href="#skills">Skills</a>
           </li>
           <li>
-            {/* <NavLink to="/contact" activeClassName="active">Contact</NavLink> */}
             <a href="#work">Work</a>
           </li>
-          {/* <li>
-            { <NavLink to="/projects" activeClassName="active">Projects</NavLink> }
-            <a href="#projects">Projects</a>
-          </li> */}
           <li>
-            {/* <NavLink to="/contact" activeClassName="active">Contact</NavLink> */}
             <a href="#contact">Contact</a>
           </li>
         </ul>
